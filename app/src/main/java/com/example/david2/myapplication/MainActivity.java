f1803f467c4dee2939ff5c1b777f6cf261e95dd7
kmaricha2 <keilerdavid2010@hotmail.com>
2018-09-14 17:19:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/david2/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.david2.myapplication;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraintlayout1);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+    }
+}
